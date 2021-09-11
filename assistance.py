050669fc98b0ad4c985855a8918e30bc6fe5b04d
Shivesh Chandra <shivesh96@gmail.com>
2021-09-11 15:46:50 +0530

    integrated google listener
    integrated google listener

--- assistance.py
+++ assistance.py
@@ -1,13 +1,15 @@
 from ast import If
 from datetime import datetime
 import pyttsx3
+import speech_recognition as sr
 
 # Video Reference
 # https://www.youtube.com/watch?v=Lp9Ftuq2sVI&t=1220s
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[0].id)
+print(voices[1].id)
+voices = engine.setProperty('voice', voices[1].id)
 
 
 def speak(audio):
@@ -28,8 +30,28 @@ def wishMe():
         speak("Good Night")
 
 
+def acceptCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, Language='en-in')
+        print("User Said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again...")
+        return "None"
+
+
 if __name__ == "__main__":
-    speak("Shivesh is a good boy")
+    # speak("Hello, I am Cybolite Assistant")
+    speak("My God is Mr. Shivash")
+    acceptCommand()
 
 
 # https://www.youtube.com/watch?v=46pBeyHKQuQ
